29e6935e8bcb455932c2bfb371f26fe8a5b0d9c7
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-08-11 15:57:46 -0500

    Add example from blog post
    For release post at https://neosmart.net/blog/2018/prettysize-for-rust/

---
+++ examples/blog.rs
@@ -0,0 +1,13 @@
+// This is the code in the release post at
+// https://neosmart.net/blog/2018/prettysize-for-rust/
+extern crate size;
+
+use size::Size;
+
+fn main() {
+    let bytes = 42 * MiB;
+    assert_eq!(bytes, 44040192);
+
+    let bytes = Size::Mebibytes(42);
+    assert_eq!(format!("{}", bytes), "42.0 MiB");
+}
