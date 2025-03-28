neosmart/prettysize-rs

commit 662032b89f60cc8b32fab5e46201901b0b08e06f

Implement `std::iter::Sum` (#15)

src/ops.rs
@@ -38,6 +38,7 @@
 
 use crate::{AsIntermediate, Intermediate, Size};
 use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+use std::iter::Sum;
 
 impl Add<Size> for Size {
     type Output = Size;
@@ -100,6 +101,18 @@ impl Sub<&Size> for &Size {
 
     fn sub(self, other: &Size) -> Self::Output {
         Size::from_bytes(self.bytes() - other.bytes())
+    }
+}
+
+impl Sum for Size {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::ZERO, |total, size| total + size)
+    }
+}
+
+impl<'a> Sum<&'a Size> for Size {
+    fn sum<I: Iterator<Item = &'a Size>>(iter: I) -> Self {
+        iter.fold(Self::ZERO, |total, size| total + size)
     }
 }
 

src/lib.rs
@@ -339,6 +339,9 @@ pub struct Size {
 }
 
 impl Size {
+    /// Zero value (0 bytes)
+    pub const ZERO: Self = Self { bytes: 0 };
+
     /// Initialize a `Size` from the provided value, in bytes. This is a constant function and may
     /// be used in a `const` context.
     ///

src/tests.rs
@@ -114,6 +114,17 @@ fn size_subtraction() {
 }
 
 #[test]
+fn size_summation() {
+    let sizes = vec![Size::from_mib(20), Size::from_mib(22)];
+
+    // sum references
+    assert_eq!(sizes.iter().sum::<Size>(), Size::Mebibytes(42));
+
+    // sum owned values
+    assert_eq!(sizes.into_iter().sum::<Size>(), Size::Mebibytes(42));
+}
+
+#[test]
 fn primitive_multiplication() {
     let size = &Size::from_gb(12) * 7;
     assert_eq!(size.bytes(), 84000000000);

commit 0bf9db2ae3153f86c87796afda65d85dc17b6e8d

Fix no_std compilation (broken by #15)

src/ops.rs
@@ -38,7 +38,7 @@
 
 use crate::{AsIntermediate, Intermediate, Size};
 use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
-use std::iter::Sum;
+use core::iter::Sum;
 
 impl Add<Size> for Size {
     type Output = Size;